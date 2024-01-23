e6bb74521abd577f8ac92688e342ff029e774961
Ido Zaks <idozaks@gmail.com>
2024-01-23 10:11:19 +0200

    add post method tests

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://bb1a9cd8-1416-4185-a5cd-01510659c99f.serverhub.tripleten-services.com"
+    API_URL: "https://224a67fe-42f1-4f8e-9694-803d1a18e9dc.serverhub.tripleten-services.com"
 }
--- tests/postHandlers.test.js
+++ tests/postHandlers.test.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
-
+const id = 2;
 const requestBody = 
 	{
 		"productsList": [
@@ -19,18 +19,49 @@ let actualStatus;
 
 test('expected status 200', async () => {
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/2/products`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/${id}/products`, {
 			method: 'POST',
 			headers: {
-			'Content-Type': 'application/json'
+				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(requestBody)
 		});
 		actualStatus = response.status;
-		console.log(response.body)
+		const responseBody = await response.json();
 	} catch (error) {
 		console.error(error);
 	}
 	expect(actualStatus).toBe(200)
 	
+});
+test("Response body matches expected body", async() => {
+	let responseBody;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/${id}/products`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+		responseBody = await response.json();
+	} catch {
+		console.error(error);
+	}
+
+    //Check top-level structure
+    expect(responseBody).toHaveProperty('id');
+    expect(responseBody).toHaveProperty('name');
+    expect(responseBody).toHaveProperty('productsList');
+    expect(responseBody).toHaveProperty('productsCount');
+
+	// Check structure of each product in productsList
+    responseBody.productsList.forEach(product => {
+        expect(product).toHaveProperty('id');
+        expect(product).toHaveProperty('name');
+        expect(product).toHaveProperty('price');
+        expect(product).toHaveProperty('weight');
+        expect(product).toHaveProperty('units');
+        expect(product).toHaveProperty('quantity');
+    });
 });
